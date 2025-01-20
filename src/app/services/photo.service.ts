8a6fdda441519106a3d92ab49da9a8eb94a52581
Aristoty <98070200+Aristoty@users.noreply.github.com>
2025-01-20 17:03:29 +0100

    Create photo.service.ts

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,28 @@
+,
+mport { Injectable } from '@angular/core';
+import {Camera, CameraResultType, CameraSource} from '@capacitor/camera';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  photos: {filepath: string; webviewPath?: string}[] = [];
+
+  async addPhoto(){
+    const photo = await Camera.getPhoto({
+      quality: 90,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+    });
+
+    this.photos.unshift({
+      filepath: '',
+      webviewPath: photo.webPath,
+    })
+
+  }
+
+  getPhotos(){
+    return this.photos;
+  }
+}
